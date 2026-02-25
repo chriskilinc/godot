ba11bba9af2bbd5c048b48932b96739b8fafd866
Christopher Kilinc <chriskilinc@gmail.com>
2026-02-25 22:08:23 +0100

    [astro] enable physics

--- astro/Player.cs
+++ astro/Player.cs
@@ -1,17 +1,44 @@
 using Godot;
 using System;
 
-public partial class Player : Sprite2D
+public partial class Player : RigidBody2D
 {
+
+    int force = 400;
+    float rotationSpeed = MathF.Tau * 0.75f; // 270 degrees per second
+
     public override void _Ready()
     {
         // Called every time the node is added to the scene.
-        // Initialization here
     }
 
-    public override void _Process(double delta)
+    public override void _PhysicsProcess(double delta)
     {
-        // Called every frame. Delta is time since last frame.
-        // Update game logic here
+        // WASD movement
+        // W = thrust forward
+        // A = rotate left
+        // D = rotate right
+        AngularVelocity = 0; // Reset angular velocity each frame
+        if (Input.IsActionPressed("thrust_forward"))
+        {
+            // Translate(new Vector2(0, -speed * (float)delta).Rotated(Rotation));
+            ApplyForce(Vector2.Up.Rotated(Rotation) * force, Vector2.Zero);
+        }
+
+        if (Input.IsActionPressed("rotate_left"))
+        {
+            // Rotate(-rotationSpeed * (float)delta);
+            AngularVelocity = -rotationSpeed;
+        }
+
+        if (Input.IsActionPressed("rotate_right"))
+        {
+            // Rotate(rotationSpeed * (float)delta);
+            AngularVelocity = rotationSpeed;
+        }
+
+        if (Input.IsActionJustPressed("fire"))
+        {
+        }
     }
 }
